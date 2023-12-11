5e519ab6a1a0cc74c8bd5cb7060b7e5bc3d56f14
Carolinasilva17 <99693031+Carolinasilva17@users.noreply.github.com>
2023-12-11 11:10:42 -0300

    Add files via upload

---
+++ APK/script.js
@@ -0,0 +1,13 @@
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", (evt)=>{
+
+})
+
+let num=10
+// window.localStorage.setItem("numero", num)
+localStorage.setItem("numero", num)
+localStorage.setItem("numero", 33)
+alert(localStorage.setItem("numero"))
